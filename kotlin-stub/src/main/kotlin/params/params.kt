8da3bc4be475ab4ba0beac231e4d3113fbcf2e68
holi <32414154@qq.com>
2017-07-25 19:00:37 +0800

    makes the @PublishedApi to @JvmSynthetic

--- kotlin-stub/src/main/kotlin/params/params.kt
+++ kotlin-stub/src/main/kotlin/params/params.kt
@@ -4,9 +4,10 @@ interface Param<out T> {
     fun <R> map(action: (T) -> R): R
 }
 
-@PublishedApi internal class Singleton<out T>(private val value: T) : Param<T> {
+private class Singleton<out T>(private val value: T) : Param<T> {
     override fun <R> map(action: (T) -> R) = action(value)
 }
 
-inline fun <reified T, R> paramOf(value: T, block: Param<T>.() -> R): R = Singleton(value).block()
+inline fun <reified T, R> paramOf(value: T, block: Param<T>.() -> R): R = singleton(value).block()
 
+@PublishedApi @JvmSynthetic internal fun <T> singleton(value: T): Param<T> = Singleton(value)
--- kotlin/src/test/java/kotlin/PublishedApiTest.java
+++ kotlin/src/test/java/kotlin/PublishedApiTest.java
@@ -2,15 +2,21 @@ package kotlin;
 
 import org.junit.Test;
 import params.Param;
+import params.ParamsKt;
+
+import java.lang.reflect.Method;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class PublishedApiTest {
     @Test
-    public void reportsWarningsByIDEAOnlyWhenAccessPublishedApiButCanBeSuppressed() throws Throwable {
-        @SuppressWarnings("KotlinInternalInJava")
-        Param<String> param = new params.Singleton<>("foo");
+    public void canNotBeAccessSyntheticPublishedApiDirectly() throws Throwable {
+        @SuppressWarnings("JavaReflectionMemberAccess")
+        Method published = ParamsKt.class.getDeclaredMethod("singleton", Object.class);
+
+        @SuppressWarnings("unchecked")
+        Param<String> param = (Param<String>) published.invoke(null, "foo");
 
         assertThat(param.map(it -> it), equalTo("foo"));
     }
