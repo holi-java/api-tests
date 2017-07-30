7693c275ce201126a7840c6654ec8397a759c400
holi <32414154@qq.com>
2017-07-31 03:49:12 +0800

    test spring prototype bean scope

--- spring/spring-core/src/test/kotlin/test/BeanScopeTest.kt
+++ spring/spring-core/src/test/kotlin/test/BeanScopeTest.kt
@@ -1,6 +1,7 @@
 package test
 
 import com.natpryce.hamkrest.assertion.assert
+import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.sameInstance
 import org.junit.Test
 import java.util.*
@@ -8,8 +9,18 @@ import java.util.*
 class BeanScopeTest {
     @Test
     fun `singleton scope by default`() {
-        val now = spring("singleton-scope.xml").run { { getBean<Date>("now") } }
+        val now = scope("singleton")
 
         assert.that(now(), sameInstance(now()))
     }
+
+    @Test
+    fun `prototype scope`() {
+        val now = scope("prototype")
+
+        assert.that(now(), !sameInstance(now()))
+        assert.that(now(), equalTo(now()))
+    }
+
+    private fun scope(scope: String) = spring(scope + "-scope.xml").run { { getBean<Date>("now") } }
 }
