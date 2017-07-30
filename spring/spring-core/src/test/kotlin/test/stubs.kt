783c48b32866e29c77148786956cdc9f7828c131
holi <32414154@qq.com>
2017-07-31 04:31:59 +0800

    makes the `Address` class and its `country` property not to be final  to enable spring aop proxy

--- spring/spring-core/src/test/kotlin/test/stubs.kt
+++ spring/spring-core/src/test/kotlin/test/stubs.kt
@@ -3,8 +3,18 @@ package test
 
 data class Flags(var flags: List<Int> = emptyList())
 
-data class User(var address: Address? = Address())
-data class Address(var country: String = "USA")
+data class User(var address: Address = Address())
+open class Address(open var country: String = "USA") {
+    override fun equals(other: Any?) = when {
+        other === this -> true
+        other is Address -> other.country == country
+        else -> false
+    }
+
+    override fun hashCode() = country.hashCode()
+
+    override fun toString() = country
+}
 
 data class DependsOn(val order: Int = next()) {
     private companion object OrderCounter {
