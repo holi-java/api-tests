f928909de05c91d8db208e7d8a641d3bf615dfc2
holi <32414154@qq.com>
2017-07-31 04:11:39 +0800

    test spring singleton bean depends on prototype bean

--- spring/spring-core/src/test/kotlin/test/BeanScopeTest.kt
+++ spring/spring-core/src/test/kotlin/test/BeanScopeTest.kt
@@ -22,5 +22,20 @@ class BeanScopeTest {
         assert.that(now(), equalTo(now()))
     }
 
+    @Test
+    fun `each singleton bean has its own prototype bean`() {
+        val context = spring("inject-prototype-to-singleton-bean.xml")
+
+        val user1 by provider { context.getBean<User>("user1") }
+        val user2 by provider { context.getBean<User>("user2") }
+
+        assert.that(user1, sameInstance(user1))
+        assert.that(user2, sameInstance(user2))
+        assert.that(user1, !sameInstance(user2))
+        assert.that(user1, equalTo(user2))
+        assert.that(user1.address, !sameInstance(user2.address))
+    }
+
     private fun scope(scope: String) = spring(scope + "-scope.xml").run { { getBean<Date>("now") } }
 }
+
--- spring/spring-core/src/test/kotlin/test/spring.kt
+++ spring/spring-core/src/test/kotlin/test/spring.kt
@@ -2,7 +2,15 @@ package test
 
 import org.springframework.context.ApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 fun spring(xml: String): ApplicationContext = ClassPathXmlApplicationContext(xml)
 
 inline fun <reified T : Any> ApplicationContext.getBean(name: String): T = getBean(name, T::class.java)
+
+fun <T> provider(block: () -> T): ReadOnlyProperty<Any?, T> {
+    return object : ReadOnlyProperty<Any?, T> {
+        override fun getValue(thisRef: Any?, property: KProperty<*>): T = block()
+    }
+}
