626f7f32cb5392572b4218a8c735cd0e5a8ee97e
holi <32414154@qq.com>
2017-08-01 06:53:54 +0800

    lazy initialized

--- jdk/src/test/java/test/design/LazyInitializingTest.java
+++ jdk/src/test/java/test/design/LazyInitializingTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.function.Supplier;
 
 import static org.hamcrest.Matchers.sameInstance;
+import static org.hamcrest.matchers.ExceptionMatchers.throwing;
 import static org.junit.Assert.assertThat;
 
 public class LazyInitializingTest {
@@ -16,8 +17,20 @@ public class LazyInitializingTest {
         assertThat(foo.get(), sameInstance(foo.get()));
     }
 
+    @Test
+    public void lazyInitialized() throws Throwable {
+        Supplier<String> foo = lazy(this::fail);
+
+        assertThat(foo::get, throwing(IllegalStateException.class));
+    }
+
+    private <T> T fail() {
+        throw new IllegalStateException();
+    }
+
     private <T> Supplier<T> lazy(Supplier<T> provider) {
-        T value = provider.get();
-        return () -> value;
+        @SuppressWarnings("unchecked")
+        T[] value = (T[]) new Object[1];
+        return () -> value[0] != null ? value[0] : (value[0] = provider.get());
     }
 }
