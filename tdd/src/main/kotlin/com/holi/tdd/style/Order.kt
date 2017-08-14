34dff8a8669e71ddcf7540d6c143f58258ab4a26
holi <32414154@qq.com>
2017-08-14 17:35:41 +0800

    refactoring

--- tdd/src/main/kotlin/com/holi/tdd/style/Order.kt
+++ tdd/src/main/kotlin/com/holi/tdd/style/Order.kt
@@ -4,10 +4,6 @@ package com.holi.tdd.style
 class Order(private val item: String, private val quantity: Int) {
     var filled = false; private set
 
-    fun fill(inventory: Inventory) {
-        if (inventory.hasEnough(item, quantity)) {
-            inventory.remove(item, quantity)
-            filled = true
-        }
-    }
+    fun fill(inventory: Inventory) = inventory.run { if (hasEnough(item, quantity)) remove(item, quantity).also { filled = true } }
+
 }
